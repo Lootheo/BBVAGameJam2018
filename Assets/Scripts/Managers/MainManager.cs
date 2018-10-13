2f29f4d783d8dd5901908c77f416da02e37793dd
Emilio Segura <esegura92@dunritegames.gg>
2018-10-13 09:49:36 -0500

    asdsasd

---
+++ Assets/Scripts/Managers/MainManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MainManager : MonoBehaviour {
+
+
+
+    private void Start()
+    {
+        DateTime cutDateTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 6, 0, 0);
+        Credit credit = new Credit(10, 10000);
+        if (DateTime.Now.Hour >= 6)
+        {
+            TimeSpan span = new TimeSpan(24, 0, 0);
+            cutDateTime.Add(span);
+            Debug.LogError(cutDateTime);
+        }
+        PlayerAccountManager.instance.SetCreditAccount(0, cutDateTime.ToString(), credit);
+        PlayerAccountManager.instance.AddPlayerGold(100);
+    }
+}
---
+++ Assets/Scripts/Test/TestTransaction.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class TestTransaction : MonoBehaviour {
+
+    
+    public Item testItem;
+    public int paydebt;
+    public int pointstoadd;
+    public int interest;
+    public int creditLimit;
+    public void setCredit()
+    {
+        PlayerAccountManager.instance.SetCreditAccount(0, DateTime.Now.ToString(), new Credit(interest, creditLimit));
+    }
+
+    public void AddPoints()
+    {
+        PlayerAccountManager.instance.AddPlayerPoints(pointstoadd);
+    }
+
+    public void PayDebt()
+    {
+        PlayerAccountManager.instance.PayCredit(paydebt);
+    }
+
+    public void SetTransactionWithCredit()
+    {  
+        PlayerAccountManager.instance.BuyWithCredit(testItem);
+    }
+
+    public void SetTransactionWithPoints()
+    {
+        PlayerAccountManager.instance.BuyWithPoints(testItem);
+    }
+
+    
+}
