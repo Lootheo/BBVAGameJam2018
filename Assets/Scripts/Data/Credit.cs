0553840f53390f5c599037b8851d16d90687a18b
Emilio Segura <esegura92@dunritegames.gg>
2018-10-12 23:47:11 -0500

    Credit Data

---
+++ Assets/Scripts/Data/Credit.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditAccountState
+{
+    private int balance;
+    private string cutDate;
+
+    private int minPayment;
+    private int availableCredit;
+
+    private Credit currentCredit;
+    private List<Transaction> transactions;
+
+    public int Balance
+    {
+        set
+        {
+            balance = value;
+            minPayment = Mathf.CeilToInt(balance * 0.1f);
+            availableCredit = currentCredit.CreditLimit - balance;
+        }
+
+        get { return balance; }
+    }
+
+    public int MinPayment
+    {
+        get { return minPayment; }
+    }
+
+    public int AvailableCredit
+    {
+        get { return availableCredit; }
+    }
+
+    public CreditAccountState(int _balance, string _cutDate, Credit _currentCredit, List<Transaction> _transactions)
+    {
+        currentCredit = _currentCredit;
+        transactions = _transactions;
+        Balance = _balance;
+        cutDate = _cutDate;
+    }
+
+    public CreditAccountState(int _balance, string _cutDate, Credit _currentCredit)
+    {
+        currentCredit = _currentCredit;
+        transactions = new List<Transaction>();
+        Balance = _balance;
+        cutDate = _cutDate;
+    }
+
+    public void UpdateCredit(Credit _currentCredit)
+    {
+        currentCredit = _currentCredit;
+        availableCredit = currentCredit.CreditLimit - balance;
+    }
+
+    public void AddTransaction(Transaction transaction)
+    {
+        transactions.Add(transaction);
+        Balance += transaction.Amount;
+    }
+
+
+}
+
+[System.Serializable]
+public class Credit
+{
+    private int interestRate;
+    private int creditLimit;
+
+    public int InterestRate
+    {
+        get { return interestRate; }
+    }
+
+    public int CreditLimit
+    {
+        get { return creditLimit; }
+    }
+
+    public Credit(int _interestRate, int _creditLimit)
+    {
+        interestRate = _interestRate;
+        creditLimit = _creditLimit;
+    }
+}
+
+[System.Serializable]
+public class Transaction
+{
+    private string description;
+    private int amount;
+    private string date;
+    
+    public string Description
+    {
+        get { return description; }
+
+    }
+
+    public int Amount
+    {
+        get { return amount; }
+    }
+
+    public string TransactionDate
+    {
+        get { return date; }
+    }
+
+    public Transaction(string _description, int _amount, string _date)
+    {
+        description = _description;
+        amount = _amount;
+        date = _date;
+    }
+
+}
