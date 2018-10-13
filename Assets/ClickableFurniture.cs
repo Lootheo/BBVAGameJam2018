cdcea1c72daa650b200f14d95998149f5f7f4b86
Manuel <lootheo7@gmail.com>
2018-10-12 21:39:44 -0500

    added more images and placehodler furniture prefab

---
+++ Assets/ClickableFurniture.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickableFurniture : MonoBehaviour {
+
+    //Furniture data
+    public string name;
+    public string description;
+    public bool purchased;
+    public Sprite image;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+}
