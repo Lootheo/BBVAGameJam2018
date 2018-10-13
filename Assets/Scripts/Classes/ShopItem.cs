36ea0a9c18ff1e4017f6247eaa0332753f0523d0
DESKTOP-3SKMJVU\Fenrir <dn.alvarez@outlook.com>
2018-10-13 00:35:21 -0500

    added shop scene for the avatar section

---
+++ Assets/Scripts/Classes/ShopItem.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopItem : MonoBehaviour {
+    public Image itemImage;
+    public Text itemText;
+}
---
+++ Assets/Scripts/StoreManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class StoreManager : MonoBehaviour {
+    public GameObject shopEntryPrefab;
+    public RectTransform scrollArea;
+    public List<Item> allItems;
+    public List<GameObject> currentShopItems;
+	// Use this for initialization
+	void Start () {
+        ShowItemsOfType(-1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ShowItemsOfType(int filter)
+    {
+        ClearShopObjects();
+        List<Item> filteredList = new List<Item>();
+        if (filter < 0)
+        {
+            filteredList = allItems;
+        }
+        else
+        {
+            ItemType type = (ItemType)filter;
+            filteredList = allItems.Where(x => x.itemType == type).ToList();
+        }
+        FillShopItems(filteredList);
+    }
+
+    void FillShopItems(List<Item> shopItems)
+    {
+        foreach (Item item in shopItems)
+        {
+            GameObject itemInstance = Instantiate(shopEntryPrefab, scrollArea);
+            ShopItem shopItem = itemInstance.GetComponent<ShopItem>();
+            shopItem.itemImage.sprite = item.itemGraphic;
+            shopItem.itemText.text = item.itemName;
+            scrollArea.sizeDelta += new Vector2(320, 0);
+            currentShopItems.Add(itemInstance);
+            //Text itemText = shopItem.
+        }
+    }
+
+    void ClearShopObjects()
+    {
+        foreach (GameObject item in currentShopItems)
+        {
+            Destroy(item);
+        }
+        currentShopItems.Clear();
+        scrollArea.sizeDelta = new Vector2(0, scrollArea.sizeDelta.y);
+    }
+}
