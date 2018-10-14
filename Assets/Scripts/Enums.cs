d4cd3898924f11bd2a6e09338f07dd8b32cc1074
Manuel <lootheo7@gmail.com>
2018-10-13 19:01:25 -0500

    moved sprite to respective foilders and aded the scriptables objects.

--- Assets/Scripts/Enums.cs
+++ Assets/Scripts/Enums.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum ItemType { Cloth, Furniture}
+public enum ItemType { Cloth, Furniture, Room, SkinColor}
 public enum ClothType { None, HairStyle, Chest, Legs, Feet, Accesory }
 public enum FurnitureType { None, Desk, Bed, Chair, Electronic}
 
