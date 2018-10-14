d89b1a91395ab6e40fdf1cb45513e00d5be562c3
Emilio Segura <esegura92@dunritegames.gg>
2018-10-14 00:16:04 -0500

    ui promo dialog

--- Assets/Scripts/Managers/MainManager.cs
+++ Assets/Scripts/Managers/MainManager.cs
@@ -1,28 +1,99 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System;
 
 public class MainManager : MonoBehaviour {
 
-
+    public UI_Dialog dialog;
+    public UI_PromoDialog promoDialog;
 
     private void Start()
     {
         DateTime cutDateTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 6, 0, 0);
         PlayerData data = SaveData.Load();
         Credit credit;
-        if (data.accountData.InterestRate != 0)
-            credit = new Credit(data.accountData.InterestRate, data.accountData.CreditLimit);
-        else
-            credit = new Credit(10, 1000);
-        if (DateTime.Now.Hour >= 6)
+        bool alreadyHaveCredit = false;
+        if (data.accountData.InterestRate != 0) // ya tiene un credito
         {
-            TimeSpan span = new TimeSpan(24, 0, 0);
-            cutDateTime.Add(span);
+            alreadyHaveCredit = true;
+            credit = new Credit(data.accountData.InterestRate, data.accountData.CreditLimit);
+            cutDateTime = DateTime.Parse(data.accountData.data.CutDate);
+        } // end if 
+        else // le creamos su primer credito
+        {
+            credit = new Credit(10, 1000);
+            if (DateTime.Now.Hour >= 6 && alreadyHaveCredit) //checar si ya paso la fecha de corte
+            {
+                TimeSpan span = new TimeSpan(24, 0, 0);
+                cutDateTime.Add(span);
+            }// end if
         }
-        
+
         PlayerAccountManager.instance.SetCreditAccount(data.accountData.data.Balance, cutDateTime.ToString(), credit);
         PlayerAccountManager.instance.AddPlayerGold(data.accountData.Gold);
+
+        if (DateTime.Now.Hour >= 6 && alreadyHaveCredit) //checar si ya paso la fecha de corte
+        {
+            PlayerAccountManager.instance.MakeDailyCutoff();
+            //avisar que se realizo el corte diario
+            if(PlayerAccountManager.instance.CreditAccount.Balance == 0)
+            {
+                //ofrecer mejora de credito en un dialog
+            }
+        }
+        
+
+        
     }
+
+    public void OpenDailyCutoffDialog()
+    {
+        dialog.transform.parent.gameObject.SetActive(true);
+        dialog.warningText.text = "Tu saldo al corte es de: " + PlayerAccountManager.instance.WeeklyCreditState[
+            PlayerAccountManager.instance.WeeklyCreditState.Count - 1].Balance + ".00MX. Tu nuevo saldo es de "
+            + PlayerAccountManager.instance.CreditAccount.Balance + ".00MX.";
+        dialog.confirmButton.onClick.AddListener(WatchDetails);
+        //dialog.confirmButton.onClick.AddListener(CloseWindow);
+        dialog.cancelButton.onClick.AddListener(VerifyForAugment);
+    }
+
+    public void CloseWindow()
+    {
+        dialog.transform.parent.gameObject.SetActive(false);
+    }
+
+    public void VerifyForAugment()
+    {
+        dialog.cancelButton.onClick.RemoveListener(VerifyForAugment);
+        
+        dialog.confirmButton.onClick.RemoveListener(WatchDetails);
+
+        if(PlayerAccountManager.instance.CreditAccount.Balance == 0)
+        {
+            OpenPromoDialog();
+        } // end if
+    }
+
+    public void OpenPromoDialog()
+    {
+        dialog.transform.parent.gameObject.SetActive(true);
+        dialog.warningText.text = "Felicidades! Haz completado el dia sin una deuda, por eso Bancomer te ofrece mejorar una caracteristica de tu credito si asi lo deseas.";
+        dialog.confirmButton.onClick.AddListener(OpenUpgradeCreditWindow);
+    }
+
+    public void OpenUpgradeCreditWindow()
+    {
+        dialog.confirmButton.onClick.RemoveListener(OpenUpgradeCreditWindow);
+        promoDialog.OpenPromoDialog();
+    }
+
+
+    public void WatchDetails()
+    {
+        // open another modal to watch the details of the account state
+    }
+
+    
 }
---
+++ Assets/UI_PromoDialog.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UI_PromoDialog : MonoBehaviour {
+
+    public TextMeshProUGUI currentInterest;
+    public TextMeshProUGUI nextInterest;
+    public TextMeshProUGUI currentCreditLimit;
+    public TextMeshProUGUI nextCreditLimit;
+    public Button CreditButton;
+    public Button InterestButton;
+    public Button RejectPromoButton;
+
+    public int interestUpgradeRate = 2;
+    public int creditLimitUpgradeRate = 100;
+
+    private int _nextCredit;
+    private int _nextInterest;
+
+    public void OpenPromoDialog()
+    {
+        gameObject.SetActive(true);
+        int _currentInterest = PlayerAccountManager.instance.CreditAccount.InterestRate;
+        _nextInterest = _currentInterest - interestUpgradeRate;
+        int _currentCredit = PlayerAccountManager.instance.CreditAccount.CurrentCredit.CreditLimit;
+        _nextCredit = _currentCredit + (int)(_currentCredit * (creditLimitUpgradeRate * 0.01f));
+
+        currentInterest.text = _currentInterest.ToString() + " %";
+        nextInterest.text = _nextInterest.ToString() + " %"; 
+
+        currentCreditLimit.text = _currentCredit.ToString() + ".00MX";
+        nextCreditLimit.text = _nextCredit.ToString() + ".00MX";
+
+    }
+
+    public void CreditButtonAction()
+    {
+        Credit upgradedCredit = new Credit(PlayerAccountManager.instance.CreditAccount.InterestRate, _nextCredit);
+        PlayerAccountManager.instance.SetCreditAccount(PlayerAccountManager.instance.CreditAccount.Balance,
+            PlayerAccountManager.instance.CreditAccount.CutDate,
+            upgradedCredit);
+
+    }
+
+    public void InterestButtonAction()
+    {
+        Credit upgradedCredit = new Credit(_nextInterest, PlayerAccountManager.instance.CreditAccount.CurrentCredit.CreditLimit);
+        PlayerAccountManager.instance.SetCreditAccount(PlayerAccountManager.instance.CreditAccount.Balance,
+            PlayerAccountManager.instance.CreditAccount.CutDate,
+            upgradedCredit);
+    }
+
+    public void RejectButtonAction()
+    {
+        gameObject.SetActive(false);
+    }
+
+}
